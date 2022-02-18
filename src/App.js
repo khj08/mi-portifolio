794f5675cefddd99b2bcb8e27ea019b084bb3819
khj08 <62971863+khj08@users.noreply.github.com>
2022-02-17 20:04:43 -0500

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import  "bootstrap/dist/css/bootstrap.min.css";
+import {tsParticles} from 'tsparticles';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+    <tsParticles 
+    className="particles-canvas"
+    params={{
+      particles: {
+        number: {
+          value: 0,
+          density: {
+            enable: true,
+            value_area: 900
+          }
+        },
+        shape:{
+          type: "start",
+          stroke: {
+            width: 6,
+            color: "#f9ab00"
+          }
+        }
+      }
+    }}
+    />
     <Navbar />
     <Header />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
   return (
     <div className="header-wraper">
         <div className="main-info">
             <h1>Portafolio de proyectos</h1>
+            <Typed 
+                className="typed-text"
+                strings={["Web developer", "Front-end"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+            />
+            <a href="#" className="btn-main-offer">CONTÁCTAME</a>
         </div>
     </div>
   )
